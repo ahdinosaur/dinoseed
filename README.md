e7f0cf8859241a3ba51d407ab3c4680bcd8aa851
Michael Williams <dinosaur@riseup.net>
2014-04-12 14:21:16 -0700

    add README and work on code

---
+++ README.md
@@ -0,0 +1,21 @@
+# dinoseed
+
+seed a new dinosaur project
+
+## seeds
+
+- docpad (static site)
+
+## how to
+
+### install
+
+```
+[sudo] npm install -g dinoseed
+```
+
+### run
+
+```
+dinoseed <directory> <seed>
+```
--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var metalsmith = require('metalsmith');
 var prompt = require('metalsmith-prompt');
 var templates = require('metalsmith-templates');
 
-var usage = "usage: dinoseed <seed name>";
+var usage = "usage: dinoseed <directory> <seed name>";
 
-var seedName = process.argv[2];
+var dir = process.argv[3];
+if (!dir) {
+  console.log("directory not given!");
+  console.log(usage);
+  process.exit(1);
+}
 
+var seedName = process.argv[3];
 if (!seedName) {
   console.log("seed name not given!");
   console.log(usage);
@@ -26,10 +32,11 @@ try {
 
 var cwd = process.cwd();
 var cwdRelative = path.relative(__dirname, cwd);
+var dirRelative = path.join(cwdRelative, dir);
 
 metalsmith(__dirname)
   .source(seedPath)
-  .destination(cwdRelative)
+  .destination(dirRelative)
   .use(prompt(seed.prompt))
   .use(templates(seed.templates))
   .build(function (err) {
